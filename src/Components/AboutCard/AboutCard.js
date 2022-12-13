31813a51e791f090742caa088d2f2b9c8d5fe544
Rakhomonov <abbos.raxmonov.2001@gmail.com>
2022-12-13 21:17:48 +0500

    some changes

--- src/Components/AboutCard/AboutCard.js
+++ src/Components/AboutCard/AboutCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReadMore from '../ReadMore'
 import {motion} from 'framer-motion'
 
-function AboutCard({name, position, description, img, nth}) {
+function AboutCard({name, description, img, nth}) {
     const initial = {
         opacity: 0,
         scale: 0
@@ -29,13 +29,10 @@ function AboutCard({name, position, description, img, nth}) {
                 className={'flex items-center mb-[1.5rem] gap-[1.25rem] pb-[1.5rem] border-b-[1px] border-b-black/[.3]'}>
                 <img src={img} width={'4rem'} height={'4rem'} alt={name} loading={'lazy'}
                      className={'sm:min-w-[5rem] sm:max-w-[5rem] sm:min-h-[5rem] sm:max-h-[5rem] min-w-[3.5rem] object-center object-fit rounded-full'}/>
-                <div className={'flex flex-col gap-[0.5rem]'}>
-                    <h3 className={'font-raleway font-semibold text-[1.375rem] md:text-base flex items-center gap-[0.5rem]'}>
+                <h3 className={'font-raleway font-semibold text-[1.375rem] md:text-base flex items-center gap-[0.5rem]'}>
                         <span
                             className={'inline-block bg-button-green w-[0.625rem] h-[4px] rounded-full'}></span><span>{name}</span>
-                    </h3>
-                    <p className={'text-sm font-open-sans'}>{position}</p>
-                </div>
+                </h3>
             </div>
             <ReadMore>
                 {description}
--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -6,7 +6,7 @@ function Alert({isError, message}) {
             className={`px-4 py-3 fixed z-[200] top-0 w-full ${isError ? 'bg-red-100 text-red-700' : 'bg-green-100 text-green-700'}`}
             role="alert">
             <span className="block sm:inline md:text-left text-center text-md">
-                {(isError && !message) ? 'Something went wrong, please try again later' : (isError && message) ? message : 'Your message has been sent successfully'}
+                {(isError && !message) ? 'Something went wrong, please try again later' : (isError && message) ? message : 'Your message has been successfully sent!'}
             </span>
         </div>
     )
